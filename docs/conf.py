5e202a59b9abdc38b387cd119a7231aef5633057
Maen Suleiman <maensule@amazon.com>
2020-10-16 10:17:35 -0700

    changing theme to sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+/# Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -55,7 +55,7 @@ html_logo = 'images/Site-Merch_Neuron-ML-SDK_Editorial.png'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
