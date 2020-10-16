33909802b2c43a13895f3cef39d816cce5c5d9bc
Maen Suleiman <maensule@amazon.com>
2020-10-16 08:40:41 -0700

    Removing nbsphinx support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,8 @@ release = '1.8.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon','nbsphinx','recommonmark']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon','nbsphinx','recommonmark']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon','nbsphinx','sphinx.ext.autosectionlabel','recommonmark']
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 #extensions = [ ]
