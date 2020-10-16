775503e70348c812dd81e4b7ca5aa68e2d70f6ef
Maen Suleiman <maensule@amazon.com>
2020-10-16 10:13:34 -0700

    changing master doc to index

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ sys.path.insert(0, os.path.abspath(os.path.join('.','..', '..', 'src')))
 project = 'AWS Neuron'
 copyright = '2020, Amazon Web Services'
 author = 'Maen Suleiman'
+master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
 release = '1.8.0'
